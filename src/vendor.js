79181fbf7370b3527431119cf7643a333d90b318
AtsunaAkiya <atsunaakiya@outlook.com>
2017-12-31 19:50:34 +0800

    build

--- src/vendor.js
+++ src/vendor.js
@@ -19,4 +19,5 @@ $(document).ready(function() {
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable:    true, // Choose whether you can drag to open on touch screens,
   })
+  alert("FUCK")
 });
